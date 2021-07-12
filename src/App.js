98899d8c18de22f29a510d1c5f516c625148091f
Ratul <azammmgol@gmail.com>
2021-07-12 15:24:57 +0600

    nav bar is ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import "./styles/style.css";
 import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <div>
       <Nav />
+
+      <h1>Hello there1</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
+      <h1>Hello there</h1>
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,39 +1,70 @@
-import Logo from "../assets/logo.png";
+import { useRef } from "react"
+import Logo from "../assets/logo.png"
 
 const Nav = () => {
+  const burgerMenuRef = useRef()
+  const listNavRef = useRef()
+  const navRef = useRef()
+
+  function ToggleNav() {
+    const burgerMenu = burgerMenuRef.current
+    const listNav = listNavRef.current
+
+    listNav.classList.toggle('nav-active')
+    burgerMenu.classList.toggle('toggle')
+  }
+
+  window.addEventListener("scroll", () => {
+    const nav = navRef.current
+    nav.classList.toggle("stickyNav", window.scrollY > 0)
+  })
+
   return (
-    <nav>
-      <div className="nav_left_content">
-        <div className="nav_logo_container">
+    <nav ref={navRef}>
+      <div className="nav_left_content nav_content_container">
+        <div className="nav_logo_container nav_container_item">
           <img src={Logo} alt="Main Logo" />
         </div>
 
-        <ul className="nav_links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Portfolio</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
+        <ul className="nav_links nav_text_links nav_container_item" ref={listNavRef}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#service">Services</a></li>
+          <li><a href="#project">Projects</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li> <a href="#blog">Blog</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
-    </nav>
-  );
-};
 
-export default Nav;
+      <div className="nav_right_content nav_content_container">
+        <ul className="social_links nav_container_item nav_links">
+          <li><a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook-f"></i>
+          </a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin-in"></i>
+          </a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a></li>
+        </ul>
+
+        <div className="contact_button nav_container_item">
+          <a href="#"><button>Let's Talk</button></a>
+        </div>
+      </div>
+
+      <div className="bars" ref={burgerMenuRef} onClick={ToggleNav}>
+        <div className="line1 line"></div>
+        <div className="line2 line"></div>
+        <div className="line3 line"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
